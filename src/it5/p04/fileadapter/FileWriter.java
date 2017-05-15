73cc66ae2cfb0242b0a27dcbaae7bc0a93861d8c
martin maehlmann <info-maehlmann@gmx.de>
2017-05-15 13:49:55 +0200

    intermediate ememergency push

---
+++ src/it5/p04/fileadapter/FileWriter.java
@@ -0,0 +1,7 @@
+package it5.p04.fileadapter;
+
+/**
+ * Created by martin on 15/05/17.
+ */
+public class FileWriter {
+}
---
+++ src/test/FileWriterTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by martin on 15/05/17.
+ */
+class FileWriterTest {
+
+}
