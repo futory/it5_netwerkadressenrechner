97679287bc911855ce3111673bde9a167236a3f0
martin maehlmann <info-maehlmann@gmx.de>
2017-05-19 08:33:37 +0200

    added another regex to validate ipv6 made by niklas krahl. not yet tested

--- src/org/mnm/ipv6/subnet/ipv6SubnetUtils.java
+++ src/org/mnm/ipv6/subnet/ipv6SubnetUtils.java
@@ -9,6 +9,9 @@ import java.util.stream.Stream;
 public class ipv6SubnetUtils {
 
     public static boolean isValidIP(String ipv6Address) {
+        //(([0-9a-f]{1,4}\:\:?){1,7})([0-9a-f]{4})?(([0-9]\.|1[0-9][0-9]\.|2[0-4][0-9]\.|25[0-5]\.|[0-9][0-9]\.){3}[0-9][0-9]?[0-9]?)?
+        // by Niklas Krahl, to be tested
+
         //http://stackoverflow.com/questions/53497/regular-expression-that-matches-valid-ipv6-addresses
         //kann cannot resolve ipv4 notation at the two last parts
         final String ipv6Pattern = "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))";
