7711fddba32b906ff79f9f2b79a75403dd705f37
JiFriedrich <jillbert@live.de>
2017-05-30 09:47:45 +0200

    GUI from Jilly

---
+++ test/Subpanel.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+/**
+ * Created by JiFriedrich on 29.05.2017.
+ */
+public class Subpanel {
+
+    private JTextField textField1;
+    private JTextField subnetNameTextField;
+    private JTextField textField3;
+    private JList list1;
+    private JButton createButton;
+    private JButton button2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
